b89ec6c102b92bcc48bbf6c9c5983543f935717a
DESKTOP-GYUNAM <kimkunam06@gmail.com>
2024-11-14 11:30:38 +0900

    🔧 [24-11-14 목] 2교시 Spring Boot - view-resolver (ModelAndView , RedirectAttributes)

--- chap03-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/ResolverController.java
+++ chap03-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/ResolverController.java
@@ -3,6 +3,8 @@ package com.ohgiraffers.viewresolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 // ③ 리턴 전, 처리할 메서드 클래스
 @Controller
@@ -34,6 +36,51 @@ public class ResolverController {
         *   redirect 방식은 접두사로 redirect:를 붙이면 된다. */
 
         return "redirect:/"; // : 는 redirect , 이후 /는 메인 페이지로 이동하기
+                                // MainController 의 "/" 동작 → main
             // f12 로 확인 시 string-redirect / localhost 찍힘 (갔다가 다시 옴)
+    }
+
+    @GetMapping("string-redirect-attr")
+    public String redirectAttr(RedirectAttributes attr){
+        /* comment.
+        *   redirect는 최초 요청 받게되면 응답 진행 후 다시 재요청하는 방식
+        *   따라서 request가 2번 일어나기 때문에 값을 유지할 방법이 없다.
+        *   그러므로 Servlet에서 Session, Cookie라는 개념을 학습했음.
+        *   Session과 Cookie는 별도의 공간을 할당하기 때문에
+        *   관리할 데이터가 많아지면 서버의 부담이 생길 수 있다.
+        *   스프링에서는 RedirectAttributes라는 타입을 통해
+        *   redirect 시 값을 저장할 수 있는 기능을 제공해 준다. */
+        attr.addFlashAttribute("flashMessage"
+                        , "리다이렉트시 값 유지!");
+        // Flash 해석 : 반짝.. 잠깐 넣었다 뺀다는 의미
+        /* comment. RedirectAttributes는 Session의 기능을 사용하지만,
+        *       Flash → 잠깐 값이 담겼다가 소멸되는 방식을 사용.
+        *       Session처럼 소멸될 때까지 공간을 차지하는 것이 아닌
+        *       잠시 데이터를 썼다가 사라지는 구조이기 때문에 서버의 부담이 없다. */
+        // 반환하면 데이터 없어짐. 무궁무진하게 사용 가능.
+
+        return "redirect:/";
+    }
+
+    /* comment. Model과 View를 합친 개념이다.
+    *   값을 집어 넣을수도 있고, 화면을 결정 지을수도 있다. */
+    @GetMapping("modelandview")
+    public ModelAndView modelAndViewReturn (ModelAndView mv) {
+        // Model 객체처럼 화면에 쓰일 데이터를 넣을 수 있다.
+        mv.addObject("forwardMessage",
+            "ModelAndView를 이용하여 반환");
+
+        // 문자열로 이동하고 싶은 view 페이지 지정
+        mv.setViewName("result");
+        return mv;
+    }
+
+    @GetMapping("modelandview-redirect-attr")
+    public ModelAndView moRedirectAttr (ModelAndView mv, RedirectAttributes attr){
+        attr.addFlashAttribute("flashMessage2",
+                        "ModelAndView 리다이렉트 시 값 유지!");
+
+        mv.setViewName("redirect:/");
+        return mv;
     }
 }
